fc34b27f0aa8be77fbce0c45fc7a485761cd8b09
Gabo-117 <l19290639@cdguzman.tecnm.mx>
2024-09-06 07:44:57 -0600

    Se actualizo contarpalabras.rb

--- contarpalabras.rb
+++ contarpalabras.rb
@@ -2,6 +2,10 @@ class PalabraContador
   def initialize(texto)
     @texto = texto
   end
+  def actualizar_texto(nuevo_texto)
+    @texto = nuevo_texto
+  end
+
 
   def contar_palabras_repetidas
     # Dividir el texto en palabras usando el espacio como delimitador
@@ -20,12 +24,25 @@ class PalabraContador
   end
 end
 
-# Ejemplo de uso:
 texto = "Perro perro perro hola hola la la la si no no pelota pelota."
 contador = PalabraContador.new(texto)
 resultados = contador.contar_palabras_repetidas
 
 puts "Palabras repetidas:"
 resultados.each do |palabra, cantidad|
+  puts "#{palabra}: #{cantidad}"
+end
+
+# Solicitar al usuario que ingrese un nuevo texto
+puts "\nPor favor, ingrese el nuevo texto:"
+nuevo_texto = gets.chomp  # Leemos la entrada del usuario y eliminamos cualquier salto de línea
+
+# Actualizar el texto
+contador.actualizar_texto(nuevo_texto)
+
+# Contar palabras repetidas en el nuevo texto
+resultados_actualizados = contador.contar_palabras_repetidas
+puts "\nPalabras repetidas y sus conteos (texto actualizado):"
+resultados_actualizados.each do |palabra, cantidad|
   puts "#{palabra}: #{cantidad}"
 end
